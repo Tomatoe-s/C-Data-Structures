6b95ae74e289a57fffbe28ac8b67e66865cb453d
yovu <7omatoe.s@gmail.com>
2025-07-11 14:39:20 +0300

    vector

--- Vector.hpp
+++ Vector.hpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <utility>
-using namespace std;
 
 
 
@@ -34,11 +33,9 @@ void push_back(Type value){
     if (vec.current_size_ >= vec.capacity_) {
         size_t new_capacity = vec.capacity_*=2;
         vec.capacity_ = new_capacity;
-        cout << "Resized array, new capacity: " <<new_capacity << endl;
         Type* newdata_ = new Type[new_capacity];
         for (int index=0;index<=vec.current_size_;index++){
             newdata_[index] = vec.data_[index];
-            cout << index<<":  "<< newdata_[index]<< endl;
 
         };
         delete[] vec.data_;
@@ -54,7 +51,7 @@ void push_back(Type value){
     
     void remove(size_t index){
         for (;index<vec.current_size_;index++){
-            vec.data_[index] = move(vec.data_[index+1]);
+            vec.data_[index] = std::move(vec.data_[index+1]);
         }
     };
     
@@ -69,19 +66,3 @@ vector_<Type> vec;
 
 
 
-int main(){
-    Vector<int> vec;
-    
-    int max_elements = 33;
-    for (int element=0;element<max_elements;element++){
-            vector_<int>* ourVec = vec.getvec();
-    int* array = ourVec->data_;
-
-        vec.push_back(element);
-        
-        cout << vec[element] << endl;
-    };
-   
-
-    return 0;
-}
